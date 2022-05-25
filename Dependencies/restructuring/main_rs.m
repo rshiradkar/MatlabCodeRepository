e6105e575ae05417a24b186cf558754d7f07420e
rshiradkar <68760842+rshiradkar@users.noreply.github.com>
2022-05-25 17:48:34 -0400

    updated restructuring

--- Dependencies/restructuring/main_rs.m
+++ Dependencies/restructuring/main_rs.m
@@ -1,23 +1,28 @@
-            
+
 dDir = pwd;
-cd('C:\Data\MRF_MRI\anon_data_prostate\');
+% cd('C:\Data\MRF_MRI\anon_data_prostate\');
 
 sourceDir = uigetdir('.','Choose a DICOM root directory');
-files = dir(sourceDir); files = files(3:end,:);
-cd(dDir);
+files = dir(sourceDir);
 
-for i = 1:length(files)
-%[opens up a folder-picker window, point it to the parent folder which contains data you wish to have anonymized]
-            %[function catalogs ALL DICOM information/tags present � no anonymization done at this step]
-            fileDir = [sourceDir '\' files(i).name];
-            dd = dicomdir(fileDir);
-            
-           
-%[function will parse above structure, delete all PHI tags, replace Pt ID with specified ID and rewrite all the data into a new folder, while maintaining organization. Organization is based on the 3rd argument above � this is the sequence of folders in which it will nest the data.]
-            %[Around line 176 is where the tag removal happens. Additional tags can be specified here and �blanked�, similar to the ones already in there]
-%             destDir   = 'C:\Data\MRF_MRI\anon_data_prostate\Data restructured\';
-destDir = 'D:\UH_RP_cohort\';
+files = files(3:end,:);
 
-            dicomdircopy_rs(fileDir, dd, destDir,{'PatientID','StudyDate','SeriesDescription','SeriesNumber'});
+if strcmp(files(1).name,'desktop.ini')
+    files = files(2:end,:);
 end
-        
+
+%%
+for i = 1:length(files)
+    %[opens up a folder-picker window, point it to the parent folder which contains data you wish to have anonymized]
+    %[function catalogs ALL DICOM information/tags present � no anonymization done at this step]
+    fileDir = [sourceDir '\' files(i).name];
+    dd = dicomdir(fileDir);
+    
+    
+    %[function will parse above structure, delete all PHI tags, replace Pt ID with specified ID and rewrite all the data into a new folder, while maintaining organization. Organization is based on the 3rd argument above � this is the sequence of folders in which it will nest the data.]
+    %[Around line 176 is where the tag removal happens. Additional tags can be specified here and �blanked�, similar to the ones already in there]
+    %             destDir   = 'C:\Data\MRF_MRI\anon_data_prostate\Data restructured\';
+    destDir = 'G:\.shortcut-targets-by-id\17Qn-XoV1vir1rFjHTQVOIm09lAtluLvl\UH_Prostate\restructured\';
+    
+    dicomdircopy_rs(fileDir, dd, destDir,{'PatientID','StudyDate','SeriesDescription','SeriesNumber'});
+end
